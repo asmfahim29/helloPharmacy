fd03eab5c65771d7f9049de10652e702f7184742
ASM Fahim <abusalehmohammadfahim@gmail.com>
2022-02-21 05:55:47 -0800

    defining class to constants page

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+class Contants{
+
+  String name;
+  String id;
+  String leaveApply;
+  String taskname;
+  String dates;
+  String designation;
+  String salary;
+
+
+  Contants(this.name, this.id, this.leaveApply, this.taskname, this.dates,
+      this.designation, this.salary);
+
+
+}
